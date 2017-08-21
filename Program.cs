0a17c851a7c21c82a673a3ec03551ffbb53376ad
REGINALDO MORIKAWA <reginaldo.morikawa@senacsp.edu.br>
2017-08-21 14:50:10 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -10,6 +10,24 @@ namespace EstruturaCondicionalSalarioMinimoIfElse
     {
         static void Main(string[] args)
         {
+            decimal salario;
+
+            Console.Write("Digite seu salário: ");
+
+            salario = Convert.ToDecimal(Console.ReadLine());
+
+            if (salario > 937m)
+
+            {
+
+                Console.Write("Seu salário é maior que o salário mínimo.");
+
+            } else
+            {
+                Console.Write("Seu salário é menor que o salário mínimo.");
+            }
+
+            Console.ReadLine();
         }
     }
 }
